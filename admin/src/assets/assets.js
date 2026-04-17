1294ab7e205e06de1319bcd8c0e85507379f8642
rx-dev <rxpineple@gmail.com>
2026-04-17 21:08:08 +0530

    commmit

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -11,6 +11,21 @@ import tick_icon from './tick_icon.svg'
 import appointments_icon from './appointments_icon.svg'
 import earning_icon from './earning_icon.svg'
 import patients_icon from './patients_icon.svg'
+import doc1 from './doc1.png'
+import doc2 from './doc2.png'
+import doc3 from './doc3.png'
+import doc4 from './doc4.png'
+import doc5 from './doc5.png'
+import doc6 from './doc6.png'
+import doc7 from './doc7.png'
+import doc8 from './doc8.png'
+import doc9 from './doc9.png'
+import doc10 from './doc10.png'
+import doc11 from './doc11.png'
+import doc12 from './doc12.png'
+import doc13 from './doc13.png'
+import doc14 from './doc14.png'
+import doc15 from './doc15.png'
 
 export const assets = {
     add_icon,
@@ -27,3 +42,21 @@ export const assets = {
     appointments_icon,
     earning_icon
 }
+
+export const doctors = [
+    { _id: 'doc1', image: doc1 },
+    { _id: 'doc2', image: doc2 },
+    { _id: 'doc3', image: doc3 },
+    { _id: 'doc4', image: doc4 },
+    { _id: 'doc5', image: doc5 },
+    { _id: 'doc6', image: doc6 },
+    { _id: 'doc7', image: doc7 },
+    { _id: 'doc8', image: doc8 },
+    { _id: 'doc9', image: doc9 },
+    { _id: 'doc10', image: doc10 },
+    { _id: 'doc11', image: doc11 },
+    { _id: 'doc12', image: doc12 },
+    { _id: 'doc13', image: doc13 },
+    { _id: 'doc14', image: doc14 },
+    { _id: 'doc15', image: doc15 },
+]
